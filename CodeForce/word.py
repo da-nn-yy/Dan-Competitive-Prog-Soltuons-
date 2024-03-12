9658692531ee6c65dd037b517c4a49c97e063ea8
da-nn-yy <db7394928@gmail.com>
2024-03-12 13:45:02 +0300

    upper and lower case word

---
+++ CodeForce/word.py
@@ -0,0 +1,14 @@
+word = input()
+upper = 0
+lower = 0
+
+for char in word:
+  if char.isupper():
+    upper += 1
+  elif char.islower():
+    lower += 1
+
+if upper == lower:
+  print(word.lower())
+else:
+  print(word.upper())
